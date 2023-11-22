b7d6600f35006798452b9ed2f6e976b6bf23679e
renzvincent2 <renz.cruzado19@gmail.com>
2023-11-22 19:40:59 +0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import bannerImg from "../images/restauranfood.jpg";
+
+const Header = () => {
+
+
+    return (
+        <header className="header">
+            <section>
+                <div className="banner">
+                    <h2>Little Lemon</h2>
+                    <h3>Chicago</h3>
+                    <p>We are family owned Mediterranean restaurant, focused on traditional receipes served
+                        with a modern twist.</p>
+                    <Link to="/booking"><button aria-label="On Click">Reserve a Table</button></Link>
+                </div>
+
+                <div className="banner-img">
+                    <img src={bannerImg} />
+                </div>
+
+            </section>
+        </header>
+    );
+};
+
+export default Header;
